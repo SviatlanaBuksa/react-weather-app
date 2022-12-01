dabf4eb626396d5fb05cf9881b5210a8c0bd3c7c
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2022-12-01 22:32:27 +0100

    Fahrenheit conversion removed

--- src/TemperatureConversion.js
+++ src/TemperatureConversion.js
@@ -3,44 +3,10 @@ import React, { useState } from "react";
 export default function TemperatureConversion(props) {
   const [unit, setUnit] = useState("celsius");
 
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return fahrenheit;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="TemperatureConversion">
-        <span className="current-temperature">{Math.round(props.celsius)}</span>
-        <span className="units">
-          °C |
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="TemperatureConversion">
-        <span className="current-temperature">{Math.round(fahrenheit())}</span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="TemperatureConversion">
+      <span className="current-temperature">{Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </div>
+  );
 }
