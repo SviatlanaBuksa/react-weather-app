b47667dbcfcad3099d975246cd08e6933b630e18
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2022-12-01 22:40:38 +0100

    Update TemperatureConversion.js

--- src/TemperatureConversion.js
+++ src/TemperatureConversion.js
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function TemperatureConversion(props) {
-  const [unit, setUnit] = useState("celsius");
-
   return (
     <div className="TemperatureConversion">
       <span className="current-temperature">{Math.round(props.celsius)}</span>
